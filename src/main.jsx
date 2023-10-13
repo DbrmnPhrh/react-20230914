0597eba722f3f273c03a322ac56b1f8a1064dac7
DbrmnPhrh <123-300@mail.ru>
2023-10-13 18:45:24 +0500

    Добавлен Redux, store и Provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
 import { ThemeProvider } from './contexts/Theme'
 import { MainPage } from './pages/Main/component'
+import store from './redux'
 import './styles/styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-	<ThemeProvider>
-		<MainPage />
-	</ThemeProvider>
+	<React.StrictMode>
+		<ThemeProvider>
+			<Provider store={store}>
+				<MainPage/>
+			</Provider>
+		</ThemeProvider>
+	</React.StrictMode>
 )
